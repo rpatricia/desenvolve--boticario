aa83188f3b45126c554ed01d934f61f94e0862de
Renata Pausini <rpatricia@outlook.com>
2022-05-11 10:57:07 -0300

    Node JS

---
+++ Semana-10/NodeJS - Crie uma API REST padronizada e escalável/api-petshop/api/index.js
@@ -0,0 +1,62 @@
+const express = require('express');// Utilizamos o express para cria a API
+const app = express();//  Instancia do express para gera uma aplicação
+const bodyParser = require('body-parser');// Utiliza o body-parser para declaraar o formato a utilizar
+const config = require('config');// Chama as configurações do banco e passa uma nova porta para o listen
+const NaoEncontrado = require('./erros/NaoEncontrado')
+const CampoInvalido = require('./erros/CampoInvalido')
+const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const formatosAceitos = require('./Serializador').formatosAceitos
+const SerializadorErro = require('./Serializador').SerializadorErro
+
+app.use(bodyParser.json());// Declara o json como formato a ser utilizado pela api
+
+app.use((requisicao, resposta, proximo) => {
+    let formatoRequisitado = requisicao.header('Accept')
+
+    if (formatoRequisitado === '*/*') {
+        formatoRequisitado = 'application/json'
+    }
+
+    if (formatosAceitos.indexOf(formatoRequisitado) === -1) {
+        resposta.status(406)
+        resposta.end()
+        return
+    }
+
+    resposta.setHeader('Content-Type', formatoRequisitado)
+    proximo()
+})
+
+const roteador = require('./rotas/fornecedores')
+app.use('/api/fornecedores', roteador);// consumindo api de rotas dentro da api principal
+
+app.use((erro, requisicao, resposta, proximo) => {
+    let status = 500
+
+    if (erro instanceof NaoEncontrado) {
+        status = 404
+    }
+
+    if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
+        status = 400
+    }
+
+    if (erro instanceof ValorNaoSuportado) {
+        status = 406
+    }
+
+    const serializador = new SerializadorErro(
+        resposta.getHeader('Content-Type')
+    )
+    resposta.status(status)
+    resposta.send(
+        serializador.serializar({
+            mensagem: erro.message,
+            id: erro.idErro
+        })
+    )
+})
+
+// A API esculta uma porta por onde recebe as requisições
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'));
